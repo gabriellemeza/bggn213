f7f35479af8fcaa3f6f97920899a2d72de1fb821
Gabrielle Meza <gnmeza@ucsd.edu>
2021-10-20 15:29:12 -0700

    Adding Class 04 and 06 to github

---
+++ Class04/Class04.R
@@ -0,0 +1,7 @@
+x<- 1:50
+plot(x)
+plot(x,sin(x))
+plot(x,sin(x), type="l")
+plot(x,sin(x),  type="l", col= "purple")
+plot(x,sin(x),  type="l", col= "purple", lwd=4)
+plot(x,sin(x),  type="l", col= "purple", lwd=4, xlab="x axis")
